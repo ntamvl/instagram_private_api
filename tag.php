aad7874e7e8566f32e5f93d2522ab6c6ff5c57b4
Tam Nguyen <ntamvl@gmail.com>
2016-06-06 15:01:59 +0700

    add tag page

---
+++ tag.php
@@ -0,0 +1,36 @@
+<?php
+
+require 'src/Instagram.php';
+
+$username = 'potterj412';
+$password = 'abcd1234';
+$debug = false;
+$tag = $_GET["tag"];
+$next_max_id = $_GET["next_max_id"];
+
+$i = new Instagram($username, $password, $debug);
+
+$query = "selfie";
+
+try {
+    $i->login();
+} catch (InstagramException $e) {
+    echo $e->getMessage();
+    exit();
+}
+
+try {
+    if ( isset($tag) ) {
+      if ( isset($next_max_id) ) {
+        $feeds = $i->tagFeed($tag, $next_max_id);
+        echo json_encode($feeds);
+      } else {
+        $feeds = $i->tagFeed($tag);
+        echo json_encode($feeds);
+      }
+    }
+
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
+

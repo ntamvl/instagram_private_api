5230091b53e005502561b63ffc42f575cedfc468
Khoi Doan <quangkhoidoan@gmail.com>
2016-06-23 15:54:55 +0700

    Add user.php to read user's information

---
+++ user.php
@@ -0,0 +1,32 @@
+<?php
+
+require 'src/Instagram.php';
+// brian.khoidoan / abcd1234
+// $username = 'potterj412';
+// $password = 'abcd1234';
+$username = 'brian.khoidoan';
+$password = 'abcd1234';
+$debug = false;
+$user = $_GET["username"];
+
+$i = new Instagram($username, $password, $debug);
+
+$query = "selfie";
+
+try {
+    $i->login();
+} catch (InstagramException $e) {
+    echo $e->getMessage();
+    exit();
+}
+
+try {
+    if ( isset($user) ) {
+      $feeds = $i->getUsernameInfo($user, $next_max_id);
+      echo json_encode($feeds);
+    }
+
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
+
